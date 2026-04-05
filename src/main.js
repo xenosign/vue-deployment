87231c0db5827aff8e382faa4dc5ef57b5972d32
kdtTetz <xenosign@naver.com>
2026-04-05 13:17:20 +0900

    fix: router 코드 주석 처리

--- src/main.js
+++ src/main.js
@@ -1,16 +1,20 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import VueGtag from 'vue-gtag';
-import router from './router';
+// import router from './router';
 
 const app = createApp(App);
 
-app.use(
-  VueGtag,
-  {
-    config: { id: 'G-B05TZS38KE' },
-  },
-  router,
-);
+app.use(VueGtag, {
+  config: { id: 'G-B05TZS38KE' },
+});
+
+// app.use(
+//   VueGtag,
+//   {
+//     config: { id: 'G-B05TZS38KE' },
+//   },
+//   router,
+// );
 
 app.mount('#app');
